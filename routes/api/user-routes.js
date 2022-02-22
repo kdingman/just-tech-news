664f02884f33ded48163e06274415a12807073e1
Kellly <kcdonlan@yahoo.com>
2022-02-21 19:54:28 -0500

    password protection

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users - All Users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1 - Returns 1 user based on req.params.id
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
