ccec8da0e8d63f3ecfb5302d37a7068ded25cdbf
Kellly <kcdonlan@yahoo.com>
2022-02-23 16:34:22 -0500

    create Vote table, created connections between Vote, Post, User tables, cleaned up code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         // Query Configuration
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            // Total Vote Count for a Post
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         // order property is assigned a nested array, orders  most recent, descending order
         order: [['created_at', 'DESC']],
         // JOIN = array of objects - to define the object reference the model and attributes
@@ -31,7 +40,14 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+             // Total Vote Count for a Post
+             [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -66,6 +82,19 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// Put /api/posts/upvote - needs to be before :id route 
+// otherwise Express will think the word upvote is a valid param for :id
+router.put('/upvote', (req, res) => {
+    // Create the Vote
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    });
 
 // Update Post Title
 router.put('/:id', (req, res) => {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users - All Users
 router.get('/', (req, res) => {
@@ -20,7 +20,24 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+            model: Post,
+            attributes: [
+                'id',
+                'title',
+                'post_url',
+                'created_at'
+            ]
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts' // when we query user we can see which posts a user created/voted on
+            }
+        ]
     })
         .then(dbUserData => {
             if(!dbUserData) {
