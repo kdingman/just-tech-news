8ded7882896c39e2ca0d4c08da1f6bd7f77670f4
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-03-01 16:02:29 -0500

    homepage created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,21 +3,46 @@
 
 const router = require('express').Router();
 
+// Import necessary modules
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
 router.get('/', (req, res) => {
-    // res.render = specifies which template we want to use
-    res.render('homepage', {
-        // taking a single post object and pass it to the homepage.handlebars template
-        // each property on the object (id, post, title, etc) becomes avail in the template using hb {{}} syntax
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlbars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post.id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // this will loop and map each Sequelize object into a serialized version of itself
+        // saving the results in a new posts array
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass as single post object into the homepage template
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
+
 
 module.exports = router;
